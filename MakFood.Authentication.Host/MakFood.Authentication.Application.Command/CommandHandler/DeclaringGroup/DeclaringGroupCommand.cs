876e0b62eef7f1631e749129750d8f92d996bb4d
AmirMahdiyar <amirmahdiyarshirijiyan@gmail.com>
2025-10-29 10:23:37 +0330

    اضافه کردن: اضافه کردن تعریف گروه توسط سوپرادمین

---
+++ MakFood.Authentication.Host/MakFood.Authentication.Application.Command/CommandHandler/DeclaringGroup/DeclaringGroupCommand.cs
@@ -0,0 +1,22 @@
+﻿using MakFood.Authentication.Application.Command.CommandHandler.Base;
+using MakFood.Authentication.Application.Command.CommandHandler.Base.Extension;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Authentication.Application.Command.CommandHandler.DeclaringGroup
+{
+    public class DeclaringGroupCommand : CommandBase, IRequest<DeclaringGroupCommandResponse>
+    {
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public override void Validate()
+        {
+            new DeclaringGroupCommandValidator().Validate(this).ThrowIfNeeded();
+
+        }
+    }
+}
