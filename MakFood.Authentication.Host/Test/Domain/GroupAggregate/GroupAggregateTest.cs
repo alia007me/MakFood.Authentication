52540085441b42fba0b7cffc3dd1d12be24bf006
AmirMahdiyar <amirmahdiyarshirijiyan@gmail.com>
2025-11-06 13:29:56 +0330

    اضافه کردن: نوشتن یونیت تست برای لایه domain

---
+++ MakFood.Authentication.Host/Test/Domain/GroupAggregate/GroupAggregateTest.cs
@@ -0,0 +1,94 @@
+﻿using MakFood.Authentication.Domain.Model.Entities;
+using MakFood.Authentication.Domain.Model.Enums;
+using MakFood.Authentication.Infraustraucture.Substructure.Base.DomainExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Authentication.UnitTest.Domain.GroupAggregate
+{
+    public class GroupAggregateTest
+    {
+        [Fact]
+        public void Group_Can_Have_Permission()
+        {
+            //Arrange
+            var sut = new Group("group", "");
+            var permission = new GroupPermission(1,2);
+
+            //Act
+            sut.AddPermissionsToGroup(permission);
+
+            //Assert
+            Assert.Equal(1,sut.Permissions.Count());
+        }
+        [Fact]
+        public void GroupName_Will_Not_Be_Empty()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.ThrowsAny<ValidationFailedDomainException>(() => new Group("",""));
+        }
+        [Fact]
+        public void Service_In_Permission_Will_Not_Be_Empty()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.ThrowsAny<ValidationFailedDomainException>(() => new Permission("", "Charge", "Charging wallet"));
+        }
+        [Fact]
+        public void Method_In_Permission_Will_Not_Be_Empty()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.ThrowsAny<ValidationFailedDomainException>(() => new Permission("Wallet", "", "Charging wallet"));
+        }
+        [Fact]
+        public void Changes_Permission_To_Active()
+        {
+            //Arrange
+            var sut = new Permission("Wallet", "Charge", "Charging wallet");
+
+            //Act
+            sut.ActivatePermission();
+
+            //Assert
+            Assert.Equal(PermissionStatus.Activated, sut.Status);
+        }
+        [Fact]
+        public void Changes_Permission_To_Deactive()
+        {
+            //Arrange
+            var sut = new Permission("Wallet", "Charge", "Charging wallet");
+
+            //Act
+            sut.DeactivatePermission();
+
+            //Assert
+            Assert.Equal(PermissionStatus.Deactivated, sut.Status);
+        }
+        [Fact]
+        public void Changes_Permission_To_Deleted()
+        {
+            //Arrange
+            var sut = new Permission("Wallet", "Charge", "Charging wallet");
+
+            //Act
+            sut.DeletePermission();
+
+            //Assert
+            Assert.Equal(PermissionStatus.Deleted, sut.Status);
+        }
+    }
+}
---
+++ MakFood.Authentication.Host/Test/Domain/UserAggregate/UserAggregateTest.cs
@@ -0,0 +1,67 @@
+﻿using MakFood.Authentication.Domain.Model.Entities;
+using MakFood.Authentication.Domain.Model.Enums;
+using MakFood.Authentication.Infraustraucture.Substructure.Base.DomainExceptions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Authentication.UnitTest.Domain.UserAggregate
+{
+    public class UserAggregateTest
+    {
+        [Fact]
+        public void User_Can_Get_Group()
+        {
+            //Arrange 
+            var sut = new User("ali","1","test@gmail.com","09123456789",Role.Customer);
+            var group = new UserGroup(1,Guid.NewGuid());
+
+
+            //Act
+            sut.AddGroupsToUser(group);
+
+
+            //Assert
+            Assert.Equal(1, sut.Groups.Count());
+
+        }
+        [Fact]
+        public void Username_Will_Not_Be_Empty()
+        {
+
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+            Assert.ThrowsAny<ValidationFailedDomainException>(() => new User("", "1", "test@gmail.com", "09123456789", Role.Customer));
+        }
+        [Fact]
+        public void Password_Will_Not_Be_Empty()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.ThrowsAny<ValidationFailedDomainException>(() => new User("ali", "", "test@gmail.com", "09123456789", Role.Customer));
+
+        }
+        [Fact]
+        public void Emails_Have_Only_Gmail_Suffix()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.ThrowsAny<ValidationFailedDomainException>(() => new User("ali", "4", "test@Yahoo.com", "09123456789", Role.Customer));
+        }
+
+    }
+}
