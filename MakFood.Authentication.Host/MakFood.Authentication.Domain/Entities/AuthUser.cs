c279c4dbb17e5e0eb7c7331859a50b50e58281d3
Ali Bahrami <ali0bahrami5@gmail.com>
2025-11-17 17:42:28 +0330

    اضافه کردن به دامین

---
+++ MakFood.Authentication.Host/MakFood.Authentication.Domain/Entities/AuthUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakFood.Authentication.Domain.Model.Entities
+{
+    public class AuthUser
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Username { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+
+        public string PasswordHash { get; set; } = default!;
+
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
